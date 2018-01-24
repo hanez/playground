b5b9a02202ca09bcafe21b79ab88c3e4a742cf2f
Johannes Findeisen <you@hanez.org>
2018-01-24 01:03:21 +0100

    added new simple test case for apscheduler

--- test2.py
+++ test2.py
@@ -1,7 +1,5 @@
-
 import random
 import time
-
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.jobstores.memory import MemoryJobStore
 from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
@@ -10,42 +8,32 @@ from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
 def handle_job(**kwargs):
     msg = "running job: " + kwargs['id'] + " interval: " + kwargs['interval']
     print(msg)
-    time.sleep(5)
+    time.sleep(59)
+    print(msg + " > finished!")
 
 
-def main():
+if __name__ == "__main__":
     jobstores = {
         'memory': MemoryJobStore()
     }
     executors = {
-        'default': ProcessPoolExecutor(10),
-        'threadpool': ThreadPoolExecutor(350)
+        'default': ThreadPoolExecutor(350),
+        'processpool': ProcessPoolExecutor(10)
     }
     job_defaults = {
-        'max_instances': 10000
+        'max_instances': 250
     }
     scheduler = BackgroundScheduler(jobstores=jobstores, executors=executors, job_defaults=job_defaults)
 
-    ''' Add jobs here '''
     x = 1
-    for x in range(1, 3500):
-        interval=random.randint(30, 120)
+    for x in range(1, 12000):
+        interval=random.randint(60, 300)
         scheduler.add_job(handle_job, 'interval', seconds=interval, kwargs={'id': str(x), 'interval': str(interval)})
         x += 1
-
-    print("\nStarting Scheduler...")
 
     scheduler.start()
 
     while True:
         time.sleep(1)
 
-    print("Scheduleder started")
-
-    print("Shutting down... please wait!")
-
     scheduler.shutdown()
-
-
-if __name__ == "__main__":
-    main()
